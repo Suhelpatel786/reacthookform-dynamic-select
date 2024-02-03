b38160028d14d9311e681c145129f7e7d6a6ca11
Suhel Patel <suhel.softcolon@gmail.com>
2024-02-03 15:04:24 +0530

    Basic styling *

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -23,7 +23,7 @@ const MainComponent = () => {
   });
 
   //form handlers
-  const { register, control, handleSubmit, formState } = form;
+  const { register, control, handleSubmit, formState, watch } = form;
 
   //form error handlers
   const { errors } = formState;
@@ -34,74 +34,120 @@ const MainComponent = () => {
     control,
   });
 
+  //selected names from the option
+  const selectedName: string[] = [];
+
+  //const get currentvalues
+  const data = watch(["data"]);
+
   //handle Submit
   const onSubmit = (data: any) => {
     console.log(data);
   };
 
   return (
-    <div>
+    <div className="flex flex-col justify-top items-center pt-[5%] w-[100vw] h-[100vh]">
+      <div className="w-[100%] flex justify-between items-center px-[5rem]">
+        <h1 className="text-white sm:text-[45px] text-[35px] font-bold ">
+          Dynamic Select Form
+        </h1>
+        <button
+          type="button"
+          className="bg-black text-[#feadb9] text-[25px] px-[2rem] py-[1rem] rounded-[10px]"
+          onClick={() => append({ name: "", surName: "" })}
+        >
+          Add Field
+        </button>
+      </div>
+
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <div>
+        <div className="w-[100%] pt-[30px]">
           {fields.map((field, index) => (
-            <div key={field.id}>
-              <select
-                {...register(`data.${index}.name`, {
-                  required: {
-                    value: true,
-                    message: `Please select your name for ${index}`,
-                  },
-                })}
-              >
-                <option value="">Please Select Your Name</option>
-
-                {nameOption.map((data, index) => (
-                  <option key={index} value={data.value}>
-                    {data.lable}
+            <div
+              key={field.id}
+              className="flex flex-col w-[100%] justify-between items-center"
+            >
+              <div className="pt-[1rem]">
+                <select
+                  className="w-[100%] bg-transparent text-white p-[1rem] sm:px-[2rem] text-[25px] outline-none border-white border-[2px] "
+                  {...register(`data.${index}.name`, {
+                    required: {
+                      value: true,
+                      message: `Please select your name for ${index}`,
+                    },
+                  })}
+                >
+                  <option className="bg-[#8d988d] text-white" value="">
+                    Please Select Your Name
                   </option>
-                ))}
-              </select>
 
-              <p className="text-[#ab4102]">
-                {errors?.data?.[index]?.name?.message}
-              </p>
+                  {nameOption.map((data, index) => (
+                    <option
+                      className="bg-[#8d988d] text-white"
+                      key={index}
+                      value={data.value}
+                    >
+                      {data.lable}
+                    </option>
+                  ))}
+                </select>
 
-              <select
-                {...register(`data.${index}.surName`, {
-                  required: {
-                    value: true,
-                    message: `Please select your surname for ${index}`,
-                  },
-                })}
-              >
-                <option value="">Please Select Your Surname</option>
-                {surNameOption.map((data, index) => (
-                  <option key={index} value={data.value}>
-                    {data.lable}
+                <p className="text-[#ab4102]">
+                  {errors?.data?.[index]?.name?.message}
+                </p>
+              </div>
+
+              <div className="pt-[1rem] sm:px-[1rem]">
+                <select
+                  className="w-[100%] bg-transparent text-white p-[1rem] sm:px-[2rem] text-[25px] outline-none border-white border-[2px] "
+                  {...register(`data.${index}.surName`, {
+                    required: {
+                      value: true,
+                      message: `Please select your surname for ${index}`,
+                    },
+                  })}
+                >
+                  <option className="bg-[#8d988d] text-white" value="">
+                    Please Select Your Surname
                   </option>
-                ))}
-              </select>
+                  {surNameOption.map((data, index) => (
+                    <option
+                      className="bg-[#8d988d] text-white"
+                      key={index}
+                      value={data.value}
+                    >
+                      {data.lable}
+                    </option>
+                  ))}
+                </select>
 
-              <p className="text-[#ab4102]">
-                {errors?.data?.[index]?.surName?.message}
-              </p>
+                <p className="text-[#ab4102]">
+                  {errors?.data?.[index]?.surName?.message}
+                </p>
+              </div>
 
               {index > 0 && (
-                <button type="button" onClick={() => remove(index)}>
-                  Remove Field
-                </button>
+                <div className="pt-[1rem]">
+                  <button
+                    type="button"
+                    onClick={() => remove(index)}
+                    className="bg-black text-[#feadb9] text-[25px] px-[2rem] py-[1rem] rounded-[10px]"
+                  >
+                    Remove Field
+                  </button>
+                </div>
               )}
             </div>
           ))}
-
+        </div>
+        <div className="pt-[1rem] flex items-center justify-center">
           <button
-            type="button"
-            onClick={() => append({ name: "", surName: "" })}
+            className="bg-black text-[#feadb9] text-[25px] px-[2rem] py-[1rem] rounded-[10px]"
+            type="submit"
           >
-            Add Field
+            Submit
           </button>
         </div>
-        <button type="submit">Submit</button>
       </form>
       <DevTool control={control} />
     </div>
